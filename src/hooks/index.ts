f5f3671ec772254ae2e30a5a4d66a35909d35258
DmitriyHrushko <dmitriyhryshko@gmail.com>
2025-10-19 19:00:16 +0300

    ✨ add useProduct and useSocket hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,7 @@
 export * from './useDebounce';
+export * from './useSocket';
 export * from './useConfirmDialog';
 export * from './useToast';
 export * from './useLocalProducts';
 export * from './useInfiniteScroll';
+export * from './useProductForm';
